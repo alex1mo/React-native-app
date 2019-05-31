f96485db9807f2a67a1886fdafc0fb98198f27e4
Aleksandr Nikulenko <11nav09alex1mo@gmail.com>
2019-05-31 21:41:55 +0300

    add SignUp in App components for test

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./src/reducer";
 import rootSaga from "./src/sagas";
+import SingnUp from "./src/pages/signup/SingnUp";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -15,6 +15,10 @@ sagaMiddleware.run(rootSaga);
 
 export default class App extends React.Component {
   render() {
-    return <Provider store={store} />;
+    return (
+      <Provider store={store}>
+        <SingnUp />
+      </Provider>
+    );
   }
 }
