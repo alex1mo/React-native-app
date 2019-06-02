465036f35e9c6c13a37b34180bef1a9f1a779e8d
Aleksandr Nikulenko <11nav09alex1mo@gmail.com>
2019-06-03 00:31:41 +0300

    added switch to Add component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,20 +6,37 @@ import Profile from "../profile/Profile";
 
 import { styles } from "./Header.style";
 
-const Header = ({ page, setPage }) => {
-  return (
-    <View style={styles.header}>
-      <TouchableOpacity onPress={() => setPage(Profile)}>
-        <Image
-          style={styles.avatar}
-          source={require("../../../assets/defaultAvatar.png")}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setPage(Add)}>
-        <Text style={styles.add}>+</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+class Header extends React.Component {
+  switchAdd = Component => {
+    if (Add === Component) {
+      return (
+        <TouchableOpacity onPress={() => this.props.setPage(null)}>
+          <Text style={styles.add}>{"<"}</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return false;
+    }
+  };
+
+  render() {
+    let { page, setPage } = this.props;
+    return (
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => setPage(Profile)}>
+          <Image
+            style={styles.avatar}
+            source={require("../../../assets/defaultAvatar.png")}
+          />
+        </TouchableOpacity>
+        {this.switchAdd(page) || (
+          <TouchableOpacity onPress={() => setPage(Add)}>
+            <Text style={styles.add}>+</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }
+}
 
 export default Header;
